fd5fac8a89a6daaf8a1395acacfd7a8db5245834
Sami Mäkelä <sami@httpster.io>
2022-10-30 03:11:15 +0300

    is it broken lulz

--- .eleventy.js
+++ .eleventy.js
@@ -49,10 +49,25 @@ module.exports = function(eleventyConfig) {
   // )
 
   return {
+    // Control which files Eleventy will process
+    // e.g.: *.md, *.njk, *.html, *.liquid
+    templateFormats: [
+      "md",
+      "njk",
+      "html",
+      "liquid"
+    ],
+
+    // Pre-process *.md files with: (default: `liquid`)
+    markdownTemplateEngine: "njk",
+
+    // Pre-process *.html files with: (default: `liquid`)
+    htmlTemplateEngine: "njk",
+    
     dir: {
-      "input": "src",
-      "output": "_site"
+      input: "src",
+      output: "_site"
     },
-    htmlTemplateEngine: "njk"
-  }
+    passthroughFileCopy: true
+  };
 }
